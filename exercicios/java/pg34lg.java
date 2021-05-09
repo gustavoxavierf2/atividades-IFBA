e8fce89892a5d0a09fbb263672b229801308374a
gustavoxavierf2 <guxfarias7@gmail.com>
2021-05-09 12:28:27 -0300

    Create pg34lg.java

---
+++ exercicios/java/pg34lg.java
@@ -0,0 +1,19 @@
+
+    import java.util.Scanner;
+
+public class pg34lg {
+    public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    double quantidade_dolar, convercao, cotacao_dolar;
+
+    System.out.print("Informe a quantidade de $: ");
+    quantidade_dolar = scan.nextDouble();
+    System.out.print("\nInforme a cotaçao do $ de hoje: ");
+    cotacao_dolar = scan.nextDouble();
+
+    convercao = quantidade_dolar * cotacao_dolar;
+
+    System.out.println("DOLAR $: " + quantidade_dolar + " = REAL R$: " + convercao);
+    } 
+}
+
